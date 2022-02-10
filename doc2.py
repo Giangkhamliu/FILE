cd73854599b9779365c16842c5af04231167742c
Giangkhamliu <grace21@navgurukul.org>
2022-02-10 09:00:04 +0530

    y first commit

--- doc2.py
+++ doc2.py
@@ -1,7 +1,15 @@
-# 2.Write a function that takes a list of numbers as argument and return the sum 
-# of all numbers which are divisible by 5. Also write a complete Python
-# program to call this function.
-
-
-
-
+# Write a program that reads a text file and count the number of words 
+# ending with "ing".
+def search():
+    f=open("test.txt","r")
+    count_ing=0
+    a=f.read().split()
+    i=0
+    while i<len(a):
+        if "ing"in a[i]:
+            count_ing+=1
+            b= a[i]
+            print(b) 
+        i+=1
+    print(count_ing)   
+search()
--- doc3.py
+++
@@ -1,3 +0,0 @@
-# 3. Write a function that takes a string as an argument and return Palindrome
-# if the string is a palindrome otherwise return Not A Palindrome. Also write 
-# a complete Python program to call this function.
